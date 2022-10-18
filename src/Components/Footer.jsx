167997125fcf75f2e6f8a0b5486cfe95e4d4e8ad
Danish <skdanish1999@gmail.com>
2022-10-19 02:10:19 +0530

    footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,7 +1,30 @@
-import React from "react";
+import { Box, Flex, Heading, Text } from "@chakra-ui/react";
+import { BsFillHeartFill } from "react-icons/bs";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <Flex flexDir="column" align="center" pos="relative">
+      <Box px={12} zIndex={1} bg="#f4f6f8">
+        <Heading as="h1">Logo</Heading>
+      </Box>
+      <Box
+        top="1.5rem"
+        pos="absolute"
+        h="1px"
+        w="50%"
+        m="auto"
+        bg="red.500"
+      ></Box>
+      <Box mt={3} align="center">
+        <Heading fontSize="xs" display="flex">
+          © 2022 All rights reserved. Made with
+          <Box as="span" ml={1} align="center" mt="3px">
+            <BsFillHeartFill color="red" />
+          </Box>
+        </Heading>
+      </Box>
+    </Flex>
+  );
 };
 
 export default Footer;
