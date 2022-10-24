b06490f385e6a1ebadf113a87e3a14eb1df51853
naresh-khatri <naresh.khatri2345@gmail.com>
2022-10-24 09:23:42 +0530

    make BlogArticleresponsive

--- src/Components/BlogArticle.jsx
+++ src/Components/BlogArticle.jsx
@@ -8,6 +8,7 @@ function BlogArticle({ post }) {
         key={post.id}
         gap="50px"
         flexDir={post.id % 2 === 0 ? "row" : "row-reverse"}
+        display={{ base: "none", md: "flex" }}
       >
         <Box
           w="34%"
@@ -23,7 +24,13 @@ function BlogArticle({ post }) {
             zIndex: "-1",
           }}
         >
-          <Image src={post.img} w="100%" maxH="250px" objectFit="cover" />
+          <Image
+            src={post.img}
+            w="100%"
+            maxH="250px"
+            objectFit="cover"
+            borderRadius={10}
+          />
         </Box>
         <Flex flexDir="column" gap="20px" w="66%" justify="space-between">
           <Link as={RouterLink} to={`/post/${post.id}`}>
@@ -35,6 +42,40 @@ function BlogArticle({ post }) {
             {post.desc}
           </Box>
           <Button w="110px" variant="outline" colorScheme="red">
+            Read More
+          </Button>
+        </Flex>
+      </Flex>
+      <Flex
+        key={post.id}
+        display={{ base: "flex", md: "none" }}
+        flexDir={"column"}
+      >
+        <Box
+          pos="relative"
+          _after={{
+            content: '""',
+            backgroundColor: "#008080",
+            width: "100%",
+            height: "100%",
+            position: "absolute",
+            top: "20px",
+            left: "-20px",
+            zIndex: "-1",
+          }}
+        >
+          <Image src={post.img} w="100%" borderRadius={15} />
+        </Box>
+        <Flex flexDir="column" justify="space-between">
+          <Link as={RouterLink} to={`/post/${post.id}`}>
+            <Heading as="h1" fontSize="4xl" mt={3}>
+              {post.title}
+            </Heading>
+          </Link>
+          <Box as="p" fontSize="1em" noOfLines={5} mt={2}>
+            {post.desc}
+          </Box>
+          <Button w="7em" mt={3} variant="outline" colorScheme="red">
             Read More
           </Button>
         </Flex>
