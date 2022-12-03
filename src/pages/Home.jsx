95935c36d3cc02662a2e1a536026335ffe793721
Danish <skdanish1999@gmail.com>
2022-12-03 21:02:39 +0530

    get posts and single post

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,32 +1,24 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
 import { Box, Flex } from "@chakra-ui/react";
+import { useLocation } from "react-router-dom";
 import BlogArticle from "../Components/BlogArticle";
+
 const Home = () => {
-  const posts = [
-    {
-      id: 1,
-      title: "Title Will be here",
-      desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not ke Aldus PageMaker including versions of Lorem Ipsum.  into electronic type",
-      img: "/image1.jpg",
-    },
-    {
-      id: 2,
-      title: "Title Will be here",
-      desc: "Description will be here",
-      img: "/image2.jpg",
-    },
-    {
-      id: 3,
-      title: "Title Will be here",
-      desc: "Description will be here",
-      img: "/image3.jpg",
-    },
-    {
-      id: 4,
-      title: "Title Will be here",
-      desc: "Description will be here",
-      img: "/image4.jpg",
-    },
-  ];
+  const [posts, setPosts] = useState([]);
+  const cat = useLocation().search;
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get(`http://localhost:8800/api/posts${cat}`);
+        setPosts(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchData();
+  }, [cat]);
+
   return (
     // @delete className
     <Box color="red.400" mt="10px" className="home">
--- src/pages/Single.jsx
+++ src/pages/Single.jsx
@@ -1,16 +1,40 @@
 import { Box, Flex, Heading, Image, Link } from "@chakra-ui/react";
+import axios from "axios";
+import moment from "moment";
+import { useContext } from "react";
+import { useEffect } from "react";
+import { useState } from "react";
 import { FiEdit, FiTrash2 } from "react-icons/fi";
 
-import { Link as RouterLink } from "react-router-dom";
+import { Link as RouterLink, useLocation } from "react-router-dom";
 import Menu from "../Components/Menu";
+import { AuthContext } from "../context/AuthContext";
 
 const Single = () => {
+  const [post, setPost] = useState({});
+  const location = useLocation();
+  const postId = location.pathname.split("/")[2];
+  const { currentUser } = useContext(AuthContext);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get(
+          `http://localhost:8800/api/posts/${postId}`
+        );
+        setPost(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchData();
+  }, [postId]);
+  console.log(post);
   return (
     <Flex gap="40px" mt={8}>
       {/* main content */}
       <Box w="70%" display="flex" flexDir="column" gap="30px">
         <Image
-          src="/image1.jpg"
+          src="https://rb.gy/fpk6t8"
           w="100%"
           h="300px"
           objectFit="cover"
@@ -27,65 +51,30 @@ const Single = () => {
           />
           <Flex flexDir="column">
             <Box as="span" fontSize="18px" fontWeight="bold">
-              John Doe
+              {post.username}
             </Box>
             <Box as="p" fontSize="16px" fontWeight="400">
-              Posted 2 days ago
+              Posted {moment(post.date).fromNow()}
             </Box>
           </Flex>
-          <Link as={RouterLink} to={`/write?edit=2`}>
-            <FiEdit size="20px" cursor="pointer" color="teal" />
-          </Link>
-          <Box>
-            <FiTrash2 size="20px" color="red" cursor="pointer" />
-          </Box>
+          {currentUser.username === post.username && (
+            <Flex>
+              <Link as={RouterLink} to={`/write?edit=2`}>
+                <FiEdit size="20px" cursor="pointer" color="teal" />
+              </Link>
+              <Box>
+                <FiTrash2 size="20px" color="red" cursor="pointer" />
+              </Box>
+            </Flex>
+          )}
         </Flex>
 
         {/* title */}
         <Heading as="h1" color="black">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
-          explicabo labore ipsum?
+          {post.title}
         </Heading>
         <Box as="p" textAlign="justify" lineHeight="30px" fontSize="18px">
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book. It has survived not only
-          five centuries, but also the leap into electronic typesetting,
-          remaining essentially unchanged. It was popularised in the 1960s with
-          the release of Letraset sheets containing Lorem Ipsum passages, and
-          more recently with desktop publishing software like Aldus PageMaker
-          including versions of Lorem Ipsum.
-          <br />
-          <br />
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book. It has survived not only
-          five centuries, but also the leap into electronic typesetting,
-          remaining essentially unchanged.
-          <br />
-          <br />
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book. It has survived not only
-          five centuries, but also the leap into electronic typesetting,
-          remaining essentially unchanged. It was popularised in the 1960s with
-          the release of Letraset sheets containing Lorem Ipsum passages, and
-          more recently with desktop publishing software like Aldus PageMaker
-          including versions of Lorem Ipsum.
-          <br />
-          <br />
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book. It has survived not only
-          five centuries, but also the leap into electronic typesetting,
-          remaining essentially unchanged. It was popularised in the 1960s with
-          the release of Letraset sheets containing Lorem Ipsum passages, and
-          more recently with desktop publishing software like Aldus PageMaker
-          including versions of Lorem Ipsum.
+          {post.desc}
         </Box>
       </Box>
       {/* menu */}
