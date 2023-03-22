56608e11f19e451d4823a067189b3a22bf86408d
Danish <skdanish1999@gmail.com>
2023-03-22 23:35:12 +0530

    delete consoles and fix jwt authorization bug

--- src/pages/Single.jsx
+++ src/pages/Single.jsx
@@ -1,12 +1,9 @@
 import { Box, Flex, Heading, Image, Link } from "@chakra-ui/react";
 import axios from "axios";
 import moment from "moment";
-import { useContext } from "react";
-import { useEffect } from "react";
-import { useState } from "react";
+import { useEffect, useState, useContext } from "react";
 import { FiEdit, FiTrash2 } from "react-icons/fi";
-
-import { Link as RouterLink, useLocation } from "react-router-dom";
+import { Link as RouterLink, useLocation, useNavigate } from "react-router-dom";
 import Menu from "../Components/Menu";
 import { AuthContext } from "../context/AuthContext";
 
@@ -28,13 +25,28 @@ const Single = () => {
     };
     fetchData();
   }, [postId]);
-  console.log(post);
+  const navigate = useNavigate();
+  const token = JSON.parse(localStorage.getItem("token"));
+  const handleDelete = async (token) => {
+    console.log("token", token);
+    try {
+      await axios.delete(`http://localhost:8800/api/posts/${postId}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      navigate("/");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <Flex gap="40px" mt={8}>
       {/* main content */}
       <Box w="70%" display="flex" flexDir="column" gap="30px">
         <Image
-          src="https://rb.gy/fpk6t8"
+          src={post?.img || "https://rb.gy/fpk6t8"}
           w="100%"
           h="300px"
           objectFit="cover"
@@ -42,27 +54,33 @@ const Single = () => {
         />
         {/* user info */}
         <Flex align="center" gap="20px">
-          <Image
-            src="/user_1.jpg"
-            w="70px"
-            h="70px"
-            objectFit="cover"
-            rounded="50%"
-          />
+          {post?.userImg && (
+            <Image
+              src={post.userImg}
+              w="70px"
+              h="70px"
+              objectFit="cover"
+              rounded="50%"
+            />
+          )}
           <Flex flexDir="column">
             <Box as="span" fontSize="18px" fontWeight="bold">
-              {post.username}
+              {post?.username}
             </Box>
             <Box as="p" fontSize="16px" fontWeight="400">
-              Posted {moment(post.date).fromNow()}
+              Posted {moment(post?.date).fromNow()}
             </Box>
           </Flex>
-          {currentUser.username === post.username && (
+          {currentUser?.username === post?.username && (
             <Flex>
               <Link as={RouterLink} to={`/write?edit=2`}>
                 <FiEdit size="20px" cursor="pointer" color="teal" />
               </Link>
-              <Box>
+              <Box
+                onClick={() => handleDelete(token)}
+                mx={3}
+                border="1px solid #000"
+              >
                 <FiTrash2 size="20px" color="red" cursor="pointer" />
               </Box>
             </Flex>
@@ -71,10 +89,10 @@ const Single = () => {
 
         {/* title */}
         <Heading as="h1" color="black">
-          {post.title}
+          {post?.title}
         </Heading>
         <Box as="p" textAlign="justify" lineHeight="30px" fontSize="18px">
-          {post.desc}
+          {post?.desc}
         </Box>
       </Box>
       {/* menu */}
